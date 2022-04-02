9bfe8eabc1414d16770dfca8050fc1f22ca1c974
AveryData <averysmith@MacBook-Pro-46.local>
2022-04-02 10:31:22 -0600

    Create VeeFriendsStreamlit.py

---
+++ VeeFriendsStreamlit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 10:21:24 2022
+
+@author: averysmith
+"""
+# Libraries 
+import streamlit as st 
+
+# My title
+st.title('VeeFriend Dashboard')
+
+st.write('This is my dashboard for streamlit and the nft hackathon')
+
+
